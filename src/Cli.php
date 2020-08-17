23b2c2bfca89835bcffa2ce65689e929fa0392e7
GiantCherry <lunev1981@mail.ru>
2020-08-17 18:18:19 +0300

    2 шаг. Аргументы и опции.

--- src/Cli.php
+++ src/Cli.php
@@ -1,3 +1,30 @@
 <?php
 
 namespace DifferenceComputer\Cli;
+
+use Docopt;
+
+function run() {
+    $doc = <<<'DOCOPT'
+    Generate diff
+    
+    Usage:
+      gendiff (-h|--help)
+      gendiff (-v|--version)
+      gendiff [--format <fmt>] <firstFile> <secondFile>
+    
+    Options:
+      -h --help                     Show this screen
+      -v --version                  Show version
+      --format <fmt>                Report format [default: pretty]
+    
+    DOCOPT;
+
+        $result = Docopt::handle($doc, array('version' => '1.0.0rc2'));
+
+        print_r($result);
+
+
+        foreach ($result as $k => $v)
+            echo $k . ': ' . json_encode($v) . PHP_EOL;
+}
