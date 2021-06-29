058ec28f147cec3c00a3c2069080669bf9063a82
GiantCherry <lunev1981@mail.ru>
2021-06-29 18:26:34 +0300

    1 шаг. Вывод справки.

--- src/Cli.php
+++ src/Cli.php
@@ -8,24 +8,24 @@ function run()
 {
     $doc = <<<'DOCOPT'
     Generate diff
-    
+
     Usage:
       gendiff (-h|--help)
       gendiff (-v|--version)
-      gendiff [--format <fmt>] <firstFile> <secondFile>
-    
+
     Options:
       -h --help                     Show this screen
       -v --version                  Show version
-      --format <fmt>                Report format [default: pretty]
-    
+
     DOCOPT;
 
-    $result = Docopt::handle($doc, array('version' => '1.0.0rc2'));
 
+    $args = Docopt::handle($doc, ['version' => '1.0']);
+
+    $result = genDiff(
+        $args['<firstFile>'],
+        $args['<secondFile>'],
+        $args['--format']
+    );
     print_r($result);
-
-    foreach ($result as $k => $v) {
-        echo $k . ': ' . json_encode($v) . PHP_EOL;
-    }
 }
