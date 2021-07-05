a9b1804d7e3f79176b54e00e6b0f3059383d5212
GiantCherry <lunev1981@mail.ru>
2021-07-05 20:59:37 +0300

    2 Аргументы и опции

--- src/Cli.php
+++ src/Cli.php
@@ -12,10 +12,12 @@ function run()
     Usage:
       gendiff (-h|--help)
       gendiff (-v|--version)
+      gendiff [--format <fmt>] <firstFile> <secondFile>
 
     Options:
       -h --help                     Show this screen
       -v --version                  Show version
+      --format <fmt>                Report format [default: stylish]
 
     DOCOPT;
 
